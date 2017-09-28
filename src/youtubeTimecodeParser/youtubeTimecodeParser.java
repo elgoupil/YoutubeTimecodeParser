2423ef2419cd709e7a5926ba59c57c9d97e53f3a
virus <virus@DESKTOP-J9IMIUH>
2017-09-28 18:29:59 +0200

    Fixed some and added an example

--- src/youtubeTimecodeParser/youtubeTimecodeParser.java
+++ src/youtubeTimecodeParser/youtubeTimecodeParser.java
@@ -5,6 +5,11 @@
  */
 package youtubeTimecodeParser;
 
+import java.net.URISyntaxException;
+import java.util.List;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URIBuilder;
+
 /**
  *
  * @author virus
@@ -15,82 +20,103 @@ public class youtubeTimecodeParser {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String timeCode = "";
-        String[] hours = timeCode.split("h");
-        String[] minutes;
-        String[] seconds;
-        String hour = null;
-        String minute = null;
-        String second = null;
-        long time = 0;
-        switch (hours.length) {
-            case 2: //1,2m3s
-                hour = hours[0];
-                minutes = hours[1].split("m");
-                switch (minutes.length) {
-                    case 2: //2,3s
-                        minute = minutes[0];
-                        seconds = minutes[1].split("s");
-                        switch (seconds.length) {
-                            case 1: //3
-                                second = seconds[0];
-                                break;
-                        }
-                        break;
-                    case 1: //3s
-                        if (minutes[0].contains("s")) {
-                            seconds = minutes[0].split("s");
-                            switch (seconds.length) {
-                                case 1: //3
-                                    second = seconds[0];
-                                    break;
-                            }
-                        } else {
-                            minute = minutes[0];
-                        }
+        try {
+            URIBuilder builder = new URIBuilder("https://youtu.be/pm36k08jQ0M?t=1h57m13s");
+            List<NameValuePair> list = builder.getQueryParams();
+            if (list != null) {
+                for (NameValuePair nameValuePair : list) {
+                    if (nameValuePair.getName().equals("t")) {
+                        String timeCode = nameValuePair.getValue();
+                        System.out.println(timeCode);
+                    }
                 }
-                break;
-            case 1: //2m3s
-                if (hours[0].contains("m") || hours[0].contains("s")) {
-                    minutes = hours[0].split("m");
-                    switch (minutes.length) {
-                        case 2: //2,3s
-                            minute = minutes[0];
-                            seconds = minutes[1].split("s");
-                            switch (seconds.length) {
-                                case 1: //3
-                                    second = seconds[0];
-                                    break;
-                            }
-                            break;
-                        case 1: //3s
-                            if (minutes[0].contains("s")) {
-                                seconds = minutes[0].split("s");
+            }
+        } catch (URISyntaxException ex) {
+            System.out.println("Url error");
+        }
+    }
+
+    public Long splitTimeCode(String timeCode) {
+        long time = 0;
+        if (timeCode != null) {
+            if (!timeCode.isEmpty()) {
+                String[] hours = timeCode.split("h");
+                String[] minutes;
+                String[] seconds;
+                String hour = null;
+                String minute = null;
+                String second = null;
+                switch (hours.length) {
+                    case 2: //1,2m3s
+                        hour = hours[0];
+                        minutes = hours[1].split("m");
+                        switch (minutes.length) {
+                            case 2: //2,3s
+                                minute = minutes[0];
+                                seconds = minutes[1].split("s");
                                 switch (seconds.length) {
                                     case 1: //3
                                         second = seconds[0];
                                         break;
                                 }
-                            } else {
-                                minute = minutes[0];
+                                break;
+                            case 1: //3s
+                                if (minutes[0].contains("s")) {
+                                    seconds = minutes[0].split("s");
+                                    switch (seconds.length) {
+                                        case 1: //3
+                                            second = seconds[0];
+                                            break;
+                                    }
+                                } else {
+                                    minute = minutes[0];
+                                }
+                        }
+                        break;
+                    case 1: //2m3s
+                        if (hours[0].contains("m") || hours[0].contains("s")) {
+                            minutes = hours[0].split("m");
+                            switch (minutes.length) {
+                                case 2: //2,3s
+                                    minute = minutes[0];
+                                    seconds = minutes[1].split("s");
+                                    switch (seconds.length) {
+                                        case 1: //3
+                                            second = seconds[0];
+                                            break;
+                                    }
+                                    break;
+                                case 1: //3s
+                                    if (minutes[0].contains("s")) {
+                                        seconds = minutes[0].split("s");
+                                        switch (seconds.length) {
+                                            case 1: //3
+                                                second = seconds[0];
+                                                break;
+                                        }
+                                    } else {
+                                        minute = minutes[0];
+                                    }
+                                    break;
                             }
-                            break;
-                    }
-                }else{
-                    hour = hours[0];
+                        } else {
+                            hour = hours[0];
+                        }
+                        break;
                 }
-                break;
+                if (hour != null) {
+                    time += Long.parseLong(hour) * 3600;
+                }
+                if (minute != null) {
+                    time += Long.parseLong(minute) * 60;
+                }
+                if (second != null) {
+                    time += Long.parseLong(second);
+                }
+                time = time * 1000;
+            }
         }
-        if (hour != null) {
-            time += Long.parseLong(hour) * 3600;
-        }
-        if (minute != null) {
-            time += Long.parseLong(minute) * 60;
-        }
-        if (second != null) {
-            time += Long.parseLong(second);
-        }
-        System.out.println(time);
+        return time;
     }
 
 }
